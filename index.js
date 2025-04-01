459d37c613aaca9db59f9c7f22936c9c9b5a46fe
nimesh-korat <nimeshkorat611@gmail.com>
2025-04-01 15:01:09 +0530

    first commit

--- index.js
+++ index.js
@@ -58,7 +58,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ["http://localhost:3000", "http://localhost:3001"],
+    origin: [
+      "http://localhost:3000",
+      "http://localhost:3001",
+      "http://localhost:5173",
+      "http://localhost:7475",
+      process.env.CLIENT_URL_1,
+      process.env.CLIENT_URL_2,
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
